5f5b8985a38341eb9542e06e5c39566049638806
TabaRivas <56893404+TabaRivas@users.noreply.github.com>
2021-02-16 12:21:36 -0300

    Taba , Con las escenas

---
+++ ConquerorOfTheSkyClient/Objects/Bullet.js
@@ -0,0 +1,42 @@
+class Bullet extends Phaser.GameObjects.Sprite {
+
+	/**
+	 * Constructor.
+	 */
+	constructor (scene, x, y)
+    {		
+        super(scene, x, y, 'bullet').setScale(2);	
+		this.incX = 0;
+		this.incY = 0;
+		this.speed = Phaser.Math.GetSpeed(1000, 1);
+		this.scene.physics.world.enable(this);
+    }
+
+    fire (avion)
+    {		
+		this.rotation = Phaser.Math.DegToRad(avion.body.rotation);
+		this.setPosition(avion.x, avion.y);
+		var angle = Phaser.Math.DegToRad(avion.body.rotation);
+		this.incX = Math.cos(angle);
+		this.incY = Math.sin(angle);
+		this.setActive(true);
+		this.setVisible(true);
+		this.lifespan = 1000;		
+    }
+
+	update(time,delta)
+	{
+		this.lifespan -= delta;
+		this.x += this.incX * (this.speed * delta);
+		this.y += this.incY * (this.speed * delta);
+
+		if (this.lifespan <= 0)
+		{
+			this.setActive(false);
+			this.setVisible(false);
+		}
+		
+	}
+}
+
+export default Bullet ;
--- ConquerorOfTheSkyClient/lib/websocket.js
+++ ConquerorOfTheSkyClient/lib/websocket.js
@@ -1,32 +1,34 @@
-function Client() {
+class Client {
+    
+    constructor() {
+    }
+    openConnection() {
+        var url = 'ws://localhost:8080/web-socket    ';
 
+        this.ws = new WebSocket(url);
+        this.connected = false;
+        this.ws.onmessage = this.onMessage.bind(this);        
+        this.ws.onerror = this.displayError.bind(this);        
+        this.ws.onopen = this.connectionOpen.bind(this);
+       
+        
+    }
+    connectionOpen() {
+        this.connected = true;     
+      //  myText.text = 'connected\n';
+    }
+    isConnected() {
+        return this.connected;
+    }
+    onMessage(message) {
+      //  myText.text = message.data;
+        var msg = JSON.parse(message.data);
+       // moverAvion({ x: msg.x, y: msg.y }, avion);
+    }
+    displayError(err) {
+        console.log("llegue2");
+        console.log('Websocketerror: ' + err);
+    }
 }
 
-Client.prototype.openConnection = function() {
-    var url ='ws://localhost:8080/web-socket    ';
-
-    this.ws = new WebSocket(url);
-    this.connected = false;
-    this.ws.onmessage = this.onMessage.bind(this);
-    this.ws.onerror = this.displayError.bind(this);
-    this.ws.onopen = this.connectionOpen.bind(this);
-};
-
-Client.prototype.connectionOpen = function() {
-    this.connected = true;
-    myText.text = 'connected\n';
-};
-
-Client.prototype.isConnected = function() {
-    return this.connected ;
-};
-
-Client.prototype.onMessage = function(message) {
-    myText.text = message.data;
-    var msg = JSON.parse(message.data);
-    moverAvion({x: msg.x, y: msg.y}, avion);
-};
-
-Client.prototype.displayError = function(err) {
-    console.log('Websocketerror: ' + err);
-};
+export default Client;
