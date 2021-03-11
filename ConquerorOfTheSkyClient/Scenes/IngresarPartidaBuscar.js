710d13bd584d82ea7c7caae15de14da04764a101
Ignacio Rama <nacho.rama8@gmail.com>
2021-03-11 17:08:18 -0300

    Nacho

---
+++ ConquerorOfTheSkyClient/Scenes/IngresarPartidaBuscar.js
@@ -0,0 +1,25 @@
+import { config,game } from '../lib/main.js';
+
+class IngresarPartidaBuscar extends Phaser.Scene
+{
+
+     /* Constuctor.
+     */
+
+    constructor() {
+        super({key: 'IngresarPartidaBuscar'});
+    }
+
+    preload() 
+    { 
+    }
+
+    create()
+    {
+        
+    }
+
+
+}
+
+export default IngresarPartidaBuscar
