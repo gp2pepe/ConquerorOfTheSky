d9c4c68504a7055847532fe1ac4476ec76dd9a72
Gabriel Pérez Pepe <gperezpepe@gmail.com>
2021-03-14 17:11:57 -0300

    cambios en valores SQL

--- ConquerorOfTheSky/src/main/resources/data-mysql.sql
+++ ConquerorOfTheSky/src/main/resources/data-mysql.sql
@@ -1,8 +1,20 @@
-insert into configuracion (id_configuracion,artilleria_cantidad,artilleria_danio,artilleria_radio_disparo,artilleria_salud,avion_combustible,
-                            avion_danio,avion_radio_vision,avion_salud,avion_velocidad,bomba_danio,bomba_radio_impacto,
-                            campo_tamaniox,campo_tamanioy,base_tamaniox,base_tamanioy,deposito_explosivos_salud,mapa_tamaniox,mapa_tamanioy,tanque_combustible_salud,torre_danio,
-                            torre_radio_disparo,torre_salud)
-                            values (1, 8, 12, 5, 80, 85,
-                            10,7,80,150,250,10,
-                            300,400,156,103,180,1446,996,180,15,
-                            8,250);
+insert into configuracion (id_configuracion,
+                            artilleria_cantidad,artilleria_danio,artilleria_radio_disparo,artilleria_salud,
+                            avion_combustible,avion_danio,avion_radio_vision,avion_salud,avion_velocidad,
+                            bomba_danio,bomba_radio_impacto,
+                            campo_tamaniox,campo_tamanioy,
+                            base_tamaniox,base_tamanioy,
+                            deposito_explosivos_salud,
+                            mapa_tamaniox,mapa_tamanioy,
+                            tanque_combustible_salud,
+                            torre_danio,torre_radio_disparo,torre_salud)
+                            values (1, 
+                            8, 10, 450, 50, 
+                            85,10,35,80,150,
+                            25,26,
+                            300,400,
+                            156,103,
+                            70,
+                            1446,996,
+                            70,
+                            20,60,90);
