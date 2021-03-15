fd43caa74dfe2220f7702a9f58a5909b87eaf701
Ignacio Rama <nacho.rama8@gmail.com>
2021-03-15 16:11:36 -0300

    Timer Pausado WinHuida

--- ConquerorOfTheSkyClient/Scenes/popups/Pausado.js
+++ ConquerorOfTheSkyClient/Scenes/popups/Pausado.js
@@ -1,5 +1,5 @@
 import { config } from '../../lib/main.js';
-var timedEvent;
+var timer;
 
 class Pausado extends Phaser.Scene
 {
@@ -24,6 +24,7 @@ class Pausado extends Phaser.Scene
 
     create() 
     {   
+		timer = 11000;
 		this.fondo = this.add.image(900,550, "fondoGuardar").setOrigin(0).setScale(1);
 		this.menuInicial =this.add.text(1082,770, 'Menu Inicial', { font: 'bold 32px Courier', fill: '#333' }).setInteractive();
 		this.partidaPausada =this.add.text(1000,620, 'Partida Pausada', { font: 'bold 32px Courier', fill: '#333' }).setScale (1.5);
@@ -33,11 +34,22 @@ class Pausado extends Phaser.Scene
 	}
 
 	update(){
+		if (timer > 0)
+		{
+			timer--;
+			console.log(timer);
+		}
+		if (timer == 0)
+		{
+			config.Partida.estado = "Jugando";
+			this.scene.resume("Play");
+			this.scene.remove('Pausado');
+			
+		}
 		if(config.Partida.estado=="Jugando"){
 			this.scene.setVisible(false, this);
 			this.scene.resume("Play");
 		}
-		else
 		if (config.Partida.hayError)
 		{	
 			if (config.Partida.mensajeError == 'Se desconecto un jugador')
