749fe18dd8de7bdfc6f6e2ad78bd3c200712064f
gp2pepe <gperezpepe@gmail.com>
2021-03-15 21:10:36 -0300

    vuelta atras

--- ConquerorOfTheSkyClient/Scenes/MenuInicial.js
+++ ConquerorOfTheSkyClient/Scenes/MenuInicial.js
@@ -28,8 +28,9 @@ class MenuInicial extends Phaser.Scene {
         this.fondoMenu = this.add.image(0, 0, "fondoMenu").setOrigin(0).setInteractive();
         this.add.image(900, 50, "titulo").setOrigin(0);
         this.sonido_fondo = this.sound.add('fondo_menu',{loop:false,volume:0.02});
-        this.sonido_click = this.sound.add('sonido_click',{loop:false});
-        //this.sonido_fondo.resume();
+        this.sonido_fondo.resume();
+        console.log('antes del sonido');
+        console.log(this.sonido_fondo);
         this.fondoMenu.on(Phaser.Input.Events.POINTER_DOWN, () =>{
             this.sonido_fondo.play();
         })
@@ -54,7 +55,7 @@ class MenuInicial extends Phaser.Scene {
                 y: 400,
                 duration: 1000
             });
-            this.sonido_click.play();
+            this.sonido_fondo.play();
         });
 
         //Se carga imagen interactiva de Buscar Partida
@@ -77,7 +78,7 @@ class MenuInicial extends Phaser.Scene {
                 y: 400,
                 duration: 1000
             });
-            this.sonido_click.play();
+            this.sonido_fondo.play();
         });
 
          //Se carga imagen interactiva de Buscar Partida
@@ -100,7 +101,7 @@ class MenuInicial extends Phaser.Scene {
                 y: 400,
                 duration: 1000
             });
-            this.sonido_click.play();
+            this.sonido_fondo.play();
         });
     }
     update(){     
